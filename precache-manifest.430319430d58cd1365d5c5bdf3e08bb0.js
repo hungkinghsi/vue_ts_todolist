f59134e1ce57c0f6ac33248e28007d82c3a9d56c
indeex <indeex@qq.com>
2018-06-30 15:59:22 +0800

    init repository

--- precache-manifest.430319430d58cd1365d5c5bdf3e08bb0.js
+++ precache-manifest.430319430d58cd1365d5c5bdf3e08bb0.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "885e42c128e25c34be164668f3f8ac84",
-    "url": "vue_ts_todolist/static/css/base.css"
+    "url": "static/css/base.css"
   },
   {
     "revision": "9ff42da51b03655c477e",
-    "url": "vue_ts_todolist/js/chunk-vendors.9ff42da5.js"
+    "url": "js/chunk-vendors.9ff42da5.js"
   },
   {
     "revision": "54d66d94307a00b8c44f",
-    "url": "vue_ts_todolist/js/app.54d66d94.js"
+    "url": "js/app.54d66d94.js"
   },
   {
     "revision": "4e51b989d1627e30055df4aaed873f6e",
-    "url": "vue_ts_todolist/index.html"
+    "url": "index.html"
   },
   {
     "revision": "518602523d596dd6a7585980ee978c02",
-    "url": "vue_ts_todolist/img/logo.51860252.png"
+    "url": "img/logo.51860252.png"
   },
   {
     "revision": "518602523d596dd6a7585980ee978c02",
-    "url": "vue_ts_todolist/favicon.png"
+    "url": "favicon.png"
   },
   {
     "revision": "54d66d94307a00b8c44f",
-    "url": "vue_ts_todolist/css/app.cc7241df.css"
+    "url": "css/app.cc7241df.css"
   }
 ];
