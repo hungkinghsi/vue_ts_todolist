3b5838c7b2232d0de195b279d926180964cd4753
indeex <indeex@qq.com>
2018-06-30 15:39:42 +0800

    init repository

---
+++ precache-manifest.430319430d58cd1365d5c5bdf3e08bb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "885e42c128e25c34be164668f3f8ac84",
+    "url": "/static/css/base.css"
+  },
+  {
+    "revision": "9ff42da51b03655c477e",
+    "url": "/js/chunk-vendors.9ff42da5.js"
+  },
+  {
+    "revision": "54d66d94307a00b8c44f",
+    "url": "/js/app.54d66d94.js"
+  },
+  {
+    "revision": "4e51b989d1627e30055df4aaed873f6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518602523d596dd6a7585980ee978c02",
+    "url": "/img/logo.51860252.png"
+  },
+  {
+    "revision": "518602523d596dd6a7585980ee978c02",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "54d66d94307a00b8c44f",
+    "url": "/css/app.cc7241df.css"
+  }
+];
